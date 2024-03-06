293f23a9d4c95c6a96478b7e98d2aeb9ecdf35a8
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2024-03-06 19:56:17 +0530

    using viewBinding & plain constraint layout

--- app/src/main/java/com/example/sign/MainActivity.kt
+++ app/src/main/java/com/example/sign/MainActivity.kt
@@ -2,48 +2,33 @@ package com.example.sign
 
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.drawToBitmap
+import com.example.sign.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val imageView: ImageView by lazy {
-        findViewById(R.id.imgView)
-    }
-
-    private val frameLayoutContainer: FrameLayout by lazy {
-        findViewById(R.id.frameLayoutContainer)
-    }
-
-    private val btnCreate: Button by lazy {
-        findViewById(R.id.btnCreate)
-    }
-
-    private val btnClear: Button by lazy {
-        findViewById(R.id.btnClear)
-    }
+    private lateinit var binding: ActivityMainBinding
 
     private lateinit var signatureView: SignatureView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        actionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
 
         signatureView = SignatureView(this).apply {
-            frameLayoutContainer.addView(this)
+            binding.frameLayoutContainer.addView(this)
         }
 
-        btnCreate.setOnClickListener {
+        binding.btnCreate.setOnClickListener {
             val bitmap = signatureView.drawToBitmap(Bitmap.Config.ARGB_8888)
-            imageView.setImageBitmap(bitmap)
+            binding.imgView.setImageBitmap(bitmap)
         }
 
-        btnClear.setOnClickListener {
+        binding.btnClear.setOnClickListener {
             signatureView.clear()
-            imageView.setImageBitmap(null)
+            binding.imgView.setImageBitmap(null)
         }
     }
 }
